e3221f58610e4093ed7cf2f5bf2040a10819fa05
sunil <sunilvaishnava.gmail.com>
2023-08-27 13:34:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import './App.css';
+import Header from './Components/Header';
+import Form from './Components/Form';
+import Todolist from './Components/Todolist';
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
+  return (
+    <div className='container'>
+      <div className='app-wrapper'>
+        <div>
+          <Header/>
+        </div> 
+        <div>
+          <Form
+          input={input}
+          setInput={setInput}
+          todos={todos}
+          setTodos={setTodos}
+          editTodo={editTodo}
+          setEditTodo={setEditTodo}
+          />
+        </div>
+        <div>
+          <Todolist todos={todos} setTodos={setTodos} setEditTodo={setEditTodo}/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1>What are you Doing Today?</h1>
+    </div>
+  )
+}
+
+export default Header
